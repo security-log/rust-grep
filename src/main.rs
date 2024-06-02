b5136c1e82bc9098cb6b2c5471b8ec69866e19bc
Sebastian Kremarik <skremarik680@alumno.unlam.edu.ar>
2024-06-02 11:28:10 -0300

    Base hard coded program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let term_search = "world";
+    let quote = "Hello world!";
+
+    for line in quote.lines() {
+        if line.contains(term_search) {
+            println!("{quote}\nHave the term {term_search}");
+        } else {
+            println!("Not found");
+        }
+    }
 }
