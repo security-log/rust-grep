dc06070933ce62dcde1bedf10198f883b7d92bf9
Sebastian Kremarik <skremarik680@alumno.unlam.edu.ar>
2024-06-16 10:12:30 -0300

    Enhanced input handling to support reading from both file and stdin

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,52 @@
+use clap::{App, Arg};
+use regex::Regex;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 fn main() {
-    let ctx_lines = 1;
-    let search_term = "Fusce";
-    let quote = "\
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Aenean in erat ut enim ultricies tristique.
-    Fusce molestie tristique lacinia.
-    Sed sed enim id elit vehicula tempus a quis neque.
-    In finibus vehicula feugiat.
-    Fusce mauris vitae nunc sem.
-    Maecenas placerat molestie bibendum.";
-    let mut tags: Vec<usize> = vec![]; // Guarda la linea donde ocurre el match.
-    let mut ctx: Vec<Vec<(usize, String)>> = vec![]; // Contiene un vec por match con la linea.
+    let args = App::new("Rust Grep")
+        .version("0.2")
+        .about("Searrchs for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .takes_value(true)
+                .required(true)
+                .help("The pattern to search."),
+        )
+        .arg(
+            Arg::with_name("input")
+                .takes_value(true)
+                .required(true)
+                .help("This is the path for the file to read"),
+        )
+        .get_matches();
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            tags.push(i); // Guarda la linea donde esta el match.
+    let path = args.value_of("input").unwrap_or("-");
 
-            let v = Vec::with_capacity(2 * ctx_lines + 1);
-            ctx.push(v); // envio el vector vacio a ctx;
-        }
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    if path == "-" {
+        // If not a file patch == "-"
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+
+        process_lines(reader, re);
+    } else {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        process_lines(reader, re);
     }
+}
 
-    if tags.is_empty() {
-        // Si el vec queda vacio, sale del programa.
-        println!("Not matched found!");
-        return;
-    }
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
 
-    for (i, line) in quote.lines().enumerate() {
-        for (j, tag) in tags.iter().enumerate() {
-            // Itera por cada match
-            let lower_bound = tag.saturating_sub(ctx_lines); // Impide que la resta sea menor a
-                                                             // cero.
-            let upper_bound = tag + ctx_lines;
-
-            if (i >= lower_bound) && (i <= upper_bound) {
-                let line_as_string = String::from(line);
-                let local_ctx = (i, line_as_string);
-                ctx[j].push(local_ctx);
-            }
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
-    }
-
-    let mut match_num: u8 = 1;
-    for local_ctx in ctx.iter() {
-        println!("\n{}° match:", match_num);
-
-        for &(i, ref line) in local_ctx.iter() {
-            let line_num = i + 1;
-            println!("{line_num}: {line}");
-        }
-        match_num += 1;
     }
 }
